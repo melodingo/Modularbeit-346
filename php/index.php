772ede5eac357ac65ea0026d2d28e9dff48a56db
NeviSenpai111 <nekasper@icloud.com>
2024-12-02 10:55:43 +0100

    Commit mini eier

---
+++ php/index.php
@@ -0,0 +1,36 @@
+<?php
+
+$servers = [
+    [
+    'name' => 'Small',
+    'cpu' => 4,
+    'ram' => 32768,
+    'sdd' => 4000,
+    'used_cpu' => 0,
+    'used_ram' => 0,
+    'used_sdd' => 0
+    ],
+    [
+    'name' => 'Medium',
+    'cpu' => 8,
+    'ram' => 65536,
+    'sdd' => 8000,
+    'used_cpu' => 0,
+    'used_ram' => 0,
+    'used_sdd' => 0
+    ],
+    [
+    'name' => 'Big',
+    'cpu' => 16,
+    'ram' => 131072,
+    'sdd' => 16000,
+    'used_cpu' => 0,
+    'used_ram' => 0,
+    'used_sdd' => 0
+
+]
+];
+
+
+
+?>
