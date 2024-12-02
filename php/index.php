3e271c3971875b9c3bf61d489b335e63d7cfc303
NeviSenpai111 <nekasper@icloud.com>
2024-12-02 11:13:51 +0100

    Commit

--- php/index.php
+++ php/index.php
@@ -26,38 +26,40 @@ class Server{
         $this->used_ram += $ram;
         $this->used_ssd += $ssd;
     }
-    public function updateResources($cpu, $ram, $ssd, $allocate){
+
+    public function updateResources($cpu, $ram, $ssd, $allocate= true){
         if($allocate){
             $this->used_cpu += $cpu;
             $this->used_ram += $ram;
             $this->used_ssd += $ssd;
         }else{
-            $this->used_cpu -= $cpu;
-            $this->used_ram -= $ram;
-            $this->used_ssd -= $ssd;
-        }   
+            $this->used_cpu = max(0, $this->used_cpu - $cpu);
+            $this->used_ram = max(0, $this->used_ram - $ram);
+            $this->used_ssd = max(0, $this->used_ssd - $ssd);
+        }
     }
 }
 
 $servers = [
     new Server('Small', 4, 32768, 4000),
     new Server('Medium', 8, 65536, 8000),
-    new Server('Large', 16, 131072, 16000),
-];
-
-    if($_SERVER['REQUEST_METHOD'] == 'POST'){
-        $requestedcpu = (int)$_POST['cpu'];
-        $requestedram = (int)$_POST['ram'];
-        $requestedssd = (int)$_POST['ssd'];
-    }
+        new Server('Large', 16, 131072, 16000)
+    ];
+    
+    $requestedcpu = (int)$_POST['cpu'];
+    $requestedram = (int)$_POST['ram'];
+    $requestedssd = (int)$_POST['ssd'];
 
     foreach($servers as $server){
         if($server->hasEnoughResources($requestedcpu, $requestedram, $requestedssd)){
-            $server->updateResources($requestedcpu, $requestedram, $requestedssd);
-            echo $server->name;
+            $server->updateResources($requestedcpu, $requestedram, $requestedssd , true);
+            echo 'Server: ' . $server->name;
             break;
         }
-    }
+        $server->updateResources($cpu, $ram, $ssd , false);
+        echo 'VM entfernt';
+}
+
 
 
 
