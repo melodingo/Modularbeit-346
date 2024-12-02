ab49b5f71e288ac421732946c5f45ace9c0454c9
NeviSenpai111 <nekasper@icloud.com>
2024-12-02 11:23:35 +0100

    Commit

--- php/index.php
+++ php/index.php
@@ -1,4 +1,19 @@
 <?php
+
+class VM {
+    public $name;
+    public $cpu;
+    public $ram;
+    public $ssd;
+
+    public function __construct($name, $cpu, $ram, $ssd) {
+        $this->name = $name;
+        $this->cpu = $cpu;
+        $this->ram = $ram;
+        $this->ssd = $ssd;
+    }
+}
+
 class Server{
     public $name;
     public $cpu;
@@ -38,31 +53,95 @@ class Server{
             $this->used_ssd = max(0, $this->used_ssd - $ssd);
         }
     }
-}
+    private $vms = [];
 
-$servers = [
-    new Server('Small', 4, 32768, 4000),
-    new Server('Medium', 8, 65536, 8000),
-        new Server('Large', 16, 131072, 16000)
-    ];
-    
-    $requestedcpu = (int)$_POST['cpu'];
-    $requestedram = (int)$_POST['ram'];
-    $requestedssd = (int)$_POST['ssd'];
-
-    foreach($servers as $server){
-        if($server->hasEnoughResources($requestedcpu, $requestedram, $requestedssd)){
-            $server->updateResources($requestedcpu, $requestedram, $requestedssd , true);
-            echo 'Server: ' . $server->name;
-            break;
+    public function allocateVM(VM $vm) {
+        if ($this->hasEnoughResources($vm->cpu, $vm->ram, $vm->ssd)) {
+            $this->useResources($vm->cpu, $vm->ram, $vm->ssd);
+            $this->vms[] = $vm;
+            return true;
         }
-        $server->updateResources($cpu, $ram, $ssd , false);
-        echo 'VM entfernt';
+        return false;
+    }
+
+    public function deallocateVM($vmName) {
+        foreach ($this->vms as $index => $vm) {
+            if ($vm->name === $vmName) {
+                $this->updateResources($vm->cpu, $vm->ram, $vm->ssd, false);
+                unset($this->vms[$index]);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public function getVMs() {
+        return $this->vms;
+    }
+}
+
+class OmniCloud {
+    private $servers = [];
+
+    public function __construct($servers) {
+        $this->servers = $servers;
+    }
+
+    public function allocateVMToBestServer(VM $vm) {
+        usort($this->servers, function($a, $b) {
+            return ($a->used_cpu + $a->used_ram + $a->used_ssd) <=> ($b->used_cpu + $b->used_ram + $b->used_ssd);
+        });
+
+        foreach ($this->servers as $server) {
+            if ($server->allocateVM($vm)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public function deallocateVMFromServer($vmName) {
+        foreach ($this->servers as $server) {
+            if ($server->deallocateVM($vmName)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public function calculateRevenue() {
+        $cpuCost = [1 => 5, 2 => 10, 4 => 18, 8 => 30, 16 => 45];
+        $ramCost = [512 => 5, 1024 => 10, 2048 => 20, 4096 => 40, 8192 => 80, 16384 => 160, 32768 => 320];
+        $ssdCost = [10 => 5, 20 => 10, 40 => 20, 80 => 40, 240 => 120, 500 => 250, 1000 => 500];
+
+        $totalRevenue = 0;
+
+        foreach ($this->servers as $server) {
+            foreach ($server->getVMs() as $vm) {
+                $totalRevenue += $cpuCost[$vm->cpu] + $ramCost[$vm->ram] + $ssdCost[$vm->ssd];
+            }
+        }
+
+        return $totalRevenue;
+    }
 }
 
 
 
 
+    $smallServer = new Server('Small', 4, 32768, 4000);
+    $mediumServer = new Server('Medium', 8, 65536, 8000);
+    $bigServer = new Server('Big', 16, 131072, 16000);
+    
+    $omniCloud = new OmniCloud([$smallServer, $mediumServer, $bigServer]);
+    
+    $vm1 = new VM('VM1', 2, 1024, 20);
+    $vm2 = new VM('VM2', 4, 2048, 40);
+    
+    $omniCloud->allocateVMToBestServer($vm1);
+    $omniCloud->allocateVMToBestServer($vm2);
+    
+    echo "Total Revenue: " . $omniCloud->calculateRevenue();
 
 
 ?>
